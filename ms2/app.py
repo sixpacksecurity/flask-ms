648dd7325518fb6f98b167264b3a66932f6d0e7c
sixpacksecurity <91971692+sixpacksecurity@users.noreply.github.com>
2022-08-31 21:54:23 +0530

    Delete ms2 directory
    deleting ms2

--- ms2/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-trufflehog_checks_api = "http://secret-service/"
-
-@app.route("/")
-def hello_world():
-    return "This is a response from the ms2 service."
